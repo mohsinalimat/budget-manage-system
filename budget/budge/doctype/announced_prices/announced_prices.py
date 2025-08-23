b787f184e4c8aedfb1a1814256d1e6719c03daa4
AhmedAbukhatwa <Ahmedabukhatwa1@gmail.com>
2025-08-23 03:45:40 +0300

    Doctype: Announced prices

---
+++ budget/budge/doctype/announced_prices/announced_prices.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, ahmed and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Announcedprices(Document):
+	pass
